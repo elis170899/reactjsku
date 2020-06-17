6ed53c86d275ef70e7d171e563fe1d4a810dcd00
Agustina Elisabeth Siahaan <elis170899@outlook.com>
2020-06-17 14:10:49 +0700

    Kuliah Web #23 Mengenal State React Js

--- aplikasireactku/src/Component/Fungsional/Home.jsx
+++ aplikasireactku/src/Component/Fungsional/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-import Blog from './Blog'
+// import Blog from './Blog'
+import Produk from '../Class/Produk';
 
 const Home = () => {
     return <div>
-        <Blog
+        {
+            <div>
+            <Produk nama="Macbook Pro 2019" stock="10" harga="53.000.000"/>
+            <Produk nama="Macbook Pro 2020" stock="15" harga="63.000.000"/>
+            <Produk nama="Macbook Pro 2021" stock="50" harga="33.000.000"/>
+            <Produk nama="Macbook Pro 2022" stock="100" harga="73.000.000"/>
+            <Produk nama="Macbook Pro 2023" stock="108" harga="43.000.000"/>
+            <Produk nama="Macbook Pro 2024" stock="200" harga="83.000.000"/>
+            </div>
+        /* <Blog
             tanggal="02 Juni 2020"
             judul="Teknologi Blockchain"
             summary="Generate Lorem Ipsum placeholder text for use in your graphic."
@@ -17,7 +27,7 @@ const Home = () => {
             tanggal="04 Juni 2020"
             judul="Design Pattern Laravel"
             summary="Generate Lorem Ipsum placeholder text for use in your graphic."
-        />
+        /> */}
 
 
     </div>
