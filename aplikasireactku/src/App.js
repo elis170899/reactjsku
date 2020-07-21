f542bd4ef7dc03684abc7835766be9deb08c815f
Agustina Elisabeth Siahaan <elis170899@outlook.com>
2020-07-21 16:06:30 +0700

    Kuliah Web #39 React Hooks useReducer + Context

--- aplikasireactku/src/App.js
+++ aplikasireactku/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, createContext, useReducer} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 //import BootstrapComp from './Component/Class/BootstrapComp';
 import NavbarComp from './Component/Fungsional/NavbarComp';
@@ -17,6 +17,32 @@ import HooksUseEffects from './Component/Hooks/Functional/HooksUseEffects';
 import { CartContext } from './CartContext';
 import ProductComp from './Component/Hooks/Functional/ProductComp';
 import HookReducer from './Component/Hooks/Functional/HookReducer';
+import Tagihan from './Component/Fungsional/Tagihan';
+
+export const keranjangContext = createContext()
+
+const initialState = {
+  jumlah : 1,
+  hargasatuan : 10000,
+  hargatotal : 10000
+}
+
+const reducer = (state,action) => {
+  switch (action. type) {
+      case 'tambah': return {
+          ...state,
+          jumlah: state.jumlah + 1,
+          hargatotal: state.hargasatuan + (state.hargasatuan * state.jumlah )
+      }
+      case 'kurang' : return {
+          ...state,
+          jumlah: state.jumlah - 1,
+          hargatotal: (state.hargasatuan + state.jumlah) - state.hargasatuan
+      }
+      default :
+          return state
+  }
+}
 
 
 //import Parent from './Component/Class/Parent';
@@ -29,11 +55,14 @@ const App = () => {
 
   const [value, setValue] = useState(0)
 
+  const[count,dispatch] = useReducer(reducer, initialState)
+
   return (
     <BrowserRouter >
       <CartContext.Provider value={{value, setValue}}>
 
         <NavbarComp />
+        <keranjangContext.Provider value={{keranjangState: count, keranjangDispatch:dispatch}}>
         <switch >
           <Route exact path="/" component={HomePage} />
           <Route exact path="/about" component={About} />
@@ -45,9 +74,11 @@ const App = () => {
           <Route exact path="/useeffects" component={HooksUseEffects} />
           <Route exact path="/produk" component={ProductComp} />
           <Route exact path="/reducer" component={HookReducer} />
+          <Route exact path="/tagihan" component={Tagihan} />
 
           {/* <Route exact path="/detail/:id"component={DetailComp}/> */}
         </switch>
+        </keranjangContext.Provider>
       </CartContext.Provider>
     </BrowserRouter>
 
